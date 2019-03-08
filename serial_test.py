30b84f0071c914507893a4e56a00a7aae7dd107e
mradhak1 <mradhak@stanford.edu>
2019-03-07 17:48:15 -0800

    Updated serial_test.py
    Added nums to array

--- serial_test.py
+++ serial_test.py
@@ -5,8 +5,7 @@ print("Program started")
 nums = []
 for i in range(100):
     num_str = ser.readline()
-    #nums.append(num_str)
-    print(num_str.decode().strip('\n'))
+    nums.append(num_str.decode().strip('\n'))
 
 print(nums)
 
